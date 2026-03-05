d8faf747a5817449a7db816f59b1c18d7faf1662
Caleb White <cdwhite3@pm.me>
2026-03-05 08:48:00 -0600

    feat: add --version and --help support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+use clap::builder::styling::AnsiColor;
+use clap::builder::Styles;
+use clap::Parser;
 use phpantom_lsp::Backend;
 use tower_lsp::{LspService, Server};
 
+const STYLES: Styles = Styles::styled()
+    .header(AnsiColor::Yellow.on_default().bold())
+    .usage(AnsiColor::Yellow.on_default().bold())
+    .literal(AnsiColor::Green.on_default().bold())
+    .placeholder(AnsiColor::Green.on_default());
+
+#[derive(Parser)]
+#[command(name = "phpantom_lsp", styles = STYLES)]
+#[command(
+    version,
+    about = "A fast and lightweight PHP Language Server Protocol implementation"
+)]
+struct Cli {}
+
 #[tokio::main]
 async fn main() {
+    Cli::parse();
+
     env_logger::init();
 
     let stdin = tokio::io::stdin();
