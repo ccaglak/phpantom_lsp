0e347e0a4924260081fafb4740f19915d0071e4d
github-action-benchmark <github@users.noreply.github.com>
2026-07-13 04:33:37 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 41be3555d129d62ed88b6f2c4315a76918bf094b

---
+++ dev/memory/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783917217815,
+  "repoUrl": "https://github.com/ccaglak/phpantom_lsp",
+  "entries": {
+    "PHPantom Memory Usage": [
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "41be3555d129d62ed88b6f2c4315a76918bf094b",
+          "message": "Container string aliases",
+          "timestamp": "2026-07-13T04:00:13+02:00",
+          "tree_id": "65b9e8c7d0e4f8570c1504efd61ef8fa0d4bc76d",
+          "url": "https://github.com/ccaglak/phpantom_lsp/commit/41be3555d129d62ed88b6f2c4315a76918bf094b"
+        },
+        "date": 1783917217416,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 39.8,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 64,
+            "unit": "MiB"
+          }
+        ]
+      }
+    ]
+  }
+}
