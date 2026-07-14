44a5fbce71253dffa9c3ec6224c9019060e31378
github-action-benchmark <github@users.noreply.github.com>
2026-07-14 03:56:53 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 755d40bd232ce5acc711ee3a18934a8043e51fad

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1783917217815,
+  "lastUpdate": 1784001413086,
   "repoUrl": "https://github.com/ccaglak/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -33,6 +33,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 64,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "755d40bd232ce5acc711ee3a18934a8043e51fad",
+          "message": "Assigning `null` to a property tracks",
+          "timestamp": "2026-07-14T04:30:51+02:00",
+          "tree_id": "149d87bf0ea3fa1179166d7ac1c3dbea5ad2ec49",
+          "url": "https://github.com/ccaglak/phpantom_lsp/commit/755d40bd232ce5acc711ee3a18934a8043e51fad"
+        },
+        "date": 1784001412204,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 41.4,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 63.8,
             "unit": "MiB"
           }
         ]
