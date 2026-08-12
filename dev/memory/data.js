c543722b68789c0ace46dee7541d6e44ed0eb3dd
github-action-benchmark <github@users.noreply.github.com>
2026-08-12 03:48:19 +0000

    add PHPantom Memory Usage (customSmallerIsBetter) benchmark result for 4b71f79e36d768e45fa28388564f62cc9818ddc1

--- dev/memory/data.js
+++ dev/memory/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1786334841839,
+  "lastUpdate": 1786506499801,
   "repoUrl": "https://github.com/ccaglak/phpantom_lsp",
   "entries": {
     "PHPantom Memory Usage": [
@@ -305,6 +305,40 @@ window.BENCHMARK_DATA = {
           {
             "name": "memory_laravel_model",
             "value": 74.4,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "committer": {
+            "email": "anders@jenbo.dk",
+            "name": "Anders Jenbo",
+            "username": "AJenbo"
+          },
+          "distinct": true,
+          "id": "4b71f79e36d768e45fa28388564f62cc9818ddc1",
+          "message": "A closure that returns the wrong thing for a `callable(...)` parameter\nis reported",
+          "timestamp": "2026-08-12T05:23:00+02:00",
+          "tree_id": "503feadbc175ad4fe3489bc35891260327e1da1f",
+          "url": "https://github.com/ccaglak/phpantom_lsp/commit/4b71f79e36d768e45fa28388564f62cc9818ddc1"
+        },
+        "date": 1786506499010,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "memory_hello_world",
+            "value": 34.4,
+            "unit": "MiB"
+          },
+          {
+            "name": "memory_laravel_model",
+            "value": 76.9,
             "unit": "MiB"
           }
         ]
